1ea2070641f7e42c34894c616e8e7a09193ba8c6
Mehmet Kaya <81460760+gafakaya@users.noreply.github.com>
2021-08-17 13:02:41 +0300

    DeleteEntityExampleCommand has been added

---
+++ Billdeer.Business/Handlers/EntityExamples/Commands/DeleteEntityExampleCommand.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Billdeer.Core.Utilities.Results;
+using Billdeer.DataAccess.Abstract;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Billdeer.Business.Handlers.EntityExamples.Commands
+{
+    public class DeleteEntityExampleCommand : IRequest<IResult>
+    {
+        public int EntityExampleId { get; set; }
+
+        private class DeleteEntityExampleCommandHandler : IRequestHandler<DeleteEntityExampleCommand, IResult>
+        {
+            private readonly IEntityExampleRepository _entityExampleRepository;
+
+            public DeleteEntityExampleCommandHandler(IEntityExampleRepository entityExampleRepository)
+            {
+                _entityExampleRepository = entityExampleRepository;
+            }
+
+            public async Task<IResult> Handle(DeleteEntityExampleCommand request, CancellationToken cancellationToken)
+            {
+                var entity = _entityExampleRepository.GetAsync(x => x.Id == request.EntityExampleId);
+
+                if (entity is null)
+                    new ErrorResult();
+
+                _entityExampleRepository.Delete(entity.Result);
+                await _entityExampleRepository.SaveChangesAsync();
+
+                return new SuccessResult();
+
+            }
+        }
+    }
+}
