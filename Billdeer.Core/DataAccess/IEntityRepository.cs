883e22ae2b713ad320407fe688f461ae5a627f0d
Mehmet Kaya <81460760+gafakaya@users.noreply.github.com>
2021-08-17 04:29:48 +0300

    Basic Repository design implemented.

---
+++ Billdeer.Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,37 @@
+﻿using Billdeer.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billdeer.Core.DataAccess
+{
+    public interface IEntityRepository<TEntity> where TEntity: class, IEntity, new()
+    {
+        TEntity Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        TEntity Update(TEntity entity);
+
+        void Delete(TEntity entity);
+        void DeleteRange(IEnumerable<TEntity> entities);
+
+        TEntity Get(Expression<Func<TEntity, bool>> expression);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> expression);
+
+        IEnumerable<TEntity> GetList(Expression<Func<TEntity, bool>> expression = null);
+        Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> expression = null);
+
+        IQueryable<TEntity> Queryable(Expression<Func<TEntity, bool>> expression);
+
+        int SaveChanges(); // UnitOfWork Patterni ayrı klasow olmadan repositoryde
+        Task<int> SaveChangesAsync();
+
+        int GetCount(Expression<Func<TEntity, bool>> expression = null);
+        Task<int> GetCountAsync(Expression<Func<TEntity, bool>> expression = null);
+
+
+    }
+}
---
+++ Billdeer.Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,7 @@
+﻿namespace Billdeer.Core.Utilities.Results
+{
+    public interface IDataResult<out T> : IResult
+    {
+        T Data { get; }
+    }
+}
