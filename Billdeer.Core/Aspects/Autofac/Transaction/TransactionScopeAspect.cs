03639f0a7253574589c676208b27d51721348936
Mehmet Kaya <81460760+gafakaya@users.noreply.github.com>
2021-09-23 19:38:23 +0300

    Performance and transaction aspects was added.

---
+++ Billdeer.Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,36 @@
+﻿using Billdeer.Core.Utilities.Interceptors;
+using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Billdeer.Core.Aspects.Autofac.Transaction
+{
+    /// <summary>
+    /// TransactionScopeAspect
+    /// </summary>
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+
+                }
+                catch (Exception e)
+                {
+                    e.ToString();
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ Billdeer.Core/Aspects/Autofac/Transaction/TransactionScopeAspectAsync.cs
@@ -0,0 +1,47 @@
+﻿using Billdeer.Core.Utilities.Interceptors;
+using Billdeer.Core.Utilities.IoC;
+using Castle.DynamicProxy;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Transactions;
+
+namespace Billdeer.Core.Aspects.Autofac.Transaction
+{
+    /// <summary>
+    /// TransactionScopeAspect
+    /// </summary>
+    public class TransactionScopeAspectAsync : MethodInterception
+    {
+        private readonly Type _dbContextType;
+
+        public TransactionScopeAspectAsync()
+        {
+        }
+
+        public void InterceptDbContext(IInvocation invocation)
+        {
+            var db = ServiceTool.ServiceProvider.GetService(_dbContextType) as DbContext;
+            using (var transactionScope = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Commit();
+                }
+                finally
+                {
+                    transactionScope.Rollback();
+                }
+            }
+        }
+
+        public override void Intercept(IInvocation invocation)
+        {
+            using (var tx = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                invocation.Proceed();
+                tx.Complete();
+            }
+        }
+    }
+}
