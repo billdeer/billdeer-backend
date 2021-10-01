62342aefd067cfb9010f3480108e77fc8717d8f0
Mehmet Kaya <81460760+gafakaya@users.noreply.github.com>
2021-10-02 02:26:18 +0300

    AdvertPackage and PackageProperty entities has been created.

---
+++ Billdeer.Entities/Concrete/AdvertPackage.cs
@@ -0,0 +1,22 @@
+﻿using Billdeer.Core.Entities.Abstract;
+using Billdeer.Core.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billdeer.Entities.Concrete
+{
+    public class AdvertPackage : EntityBase
+    {
+        public long AdvertId { get; set; }
+        public long PackagePropertyId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int DeliveryTime { get; set; }
+        public int Revision { get; set; }
+
+    }
+}
